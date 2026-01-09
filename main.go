b41e5adb67b77162cf2c66ea375f504e53013553
TiiJeiJ8 <981504692@qq.com>
2026-01-09 16:37:37 +0800

    INITIALIZATION

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "github.com/gin-gonic/gin"
+  "github.com/joho/godotenv"
+  "log"
+)
+
+func main() {
+  if err := godotenv.Load(); err != nil {
+    log.Println("No .env file found, relying on environment variables")
+  }
+
+  r := gin.Default()
+  r.GET("/health", func(c *gin.Context) {
+    c.JSON(200, gin.H{"status":"ok"})
+  })
+
+  r.Run(":3000") // 从 env 中读取 PORT 可选
+}
